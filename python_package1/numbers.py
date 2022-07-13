71897db9bd52a260eeb8538ba5e66b90ebf4ab1f
Ihor <igor.softex@ukr.net>
2022-07-13 16:44:45 +0300

    Мій 10-й коміт в PyCharm

---
+++ python_package1/numbers.py
@@ -0,0 +1,11 @@
+# Числа
+# a = 3
+# b = 4
+# print(4 / 2)
+
+# num = 1 / 3
+# print(num)
+# print('{0:4.2f}'.format(num))
+
+# print(10 / 4)
+print(10 / 4.0)
--- python_package1/tuples.py
+++ python_package1/tuples.py
@@ -2,3 +2,4 @@
 tuple = "Американа 20", "Американа 30"
 print(tuple)
 print(tuple.index('Американа 20'))
+print(tuple[0])
